b8e04041c3caa76fa9312903bf3b2c53154979b8
Hani Tomeh <hanitomehalali@gmail.com>
2024-02-26 20:57:37 +0100

    Remove Bookonline from App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GardenMap from './pages/GardenMap';
-import BookOnline from './pages/BookOnline';
 import HomePage from './pages/Home';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route path='/home' element={<HomePage/>} />
           <Route path='/GardenMap' element={<GardenMap />} />
-          <Route path='/BookOnline' element={<BookOnline/>} />
         </Routes>
       </Router>
     </>
